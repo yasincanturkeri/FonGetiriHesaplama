a3f77214e26176c66b2bf6bbe43ef1b00feaa960
yasincanturkeri <45851985+yasincanturkeri@users.noreply.github.com>
2026-07-16 15:04:22 +0300

    Update borsa_bot.py

--- borsa_bot.py
+++ borsa_bot.py
@@ -14,7 +14,7 @@ import os
 # Çalışan bir proxy adresi bulup aşağıya yazın.
 # Örnek format: "http://123.123.123.123:8080"
 # Ücretsiz proxy bulmak için: https://free-proxy-list.net/ adresini kullanabilirsiniz.
-PROXY = "http://93.113.63.11:3128"   # <--- BURAYI DEĞİŞTİRİN
+PROXY = "https://185.22.186.249:1080"   # <--- BURAYI DEĞİŞTİRİN
 # ============================================
 
 # --- CONFIG YÜKLEME ---
